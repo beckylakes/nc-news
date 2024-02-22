5b267d488f3810abd9184e50526bb9080dd7eea5
beckylakes <beckylake96@gmail.com>
2024-02-22 10:42:30 +0000

    Implemented code and testing for task 7 & fixed task 6 test to check for array instead of length

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -1,6 +1,7 @@
 const db = require("../db/connection.js");
 const { selectCommentsPerArticleID } = require("../models/comments.model.js");
 const { selectArticleById } = require("../models/articles.model.js");
+const { insertComment } = require("../models/comments.model.js");
 
 function getCommentsPerArticleID(req, res, next) {
   const { article_id } = req.params;
@@ -16,4 +17,19 @@ function getCommentsPerArticleID(req, res, next) {
     });
 }
 
-module.exports = { getCommentsPerArticleID };
+function postComment (req, res, next) {
+  const { article_id } = req.params;
+  const { username, body } = req.body;
+  return selectArticleById(article_id).then(() => {
+    return insertComment(article_id, username, body)
+  })
+    .then((comment) => {
+      console.log(comment)
+      res.status(201).send({ comment });
+    })
+    .catch((err) => {
+      next(err)
+    });
+};
+
+module.exports = { getCommentsPerArticleID, postComment };
